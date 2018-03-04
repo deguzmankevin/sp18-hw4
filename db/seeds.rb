95ad8e805bba65703276767549d08c3e5aa245ef
Kevin Deguzman <deguzmankevin@hotmail.com>
2018-03-04 00:44:44 -0800

    almost finished #5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cat.create(name: 'kitty')
+User.create(username: 'Goku', email: 'supersaiyan9000@dragonball.org')
+Todo.create()
